ddc449d89f99827a2c8a678a8398d8d5d50b27f8
zhaojunfei <773152@qq.com>
2017-09-13 11:04:41 +0800

    增加短信验证码通

--- src/main/java/com/mywjdh/logistics/logisticswechat/util/SMSUtil.java
+++ src/main/java/com/mywjdh/logistics/logisticswechat/util/SMSUtil.java
@@ -13,6 +13,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import com.alibaba.fastjson.JSON;
+import com.chuanglan.sms.request.SmsSendRequest;
+import com.chuanglan.sms.response.SmsSendResponse;
+import com.chuanglan.sms.util.ChuangLanSmsUtil;
+
 public class SMSUtil {
 
 	public static final String DEF_CHATSET = "UTF-8";
@@ -23,9 +28,16 @@ public class SMSUtil {
 
 	public static String userAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36";
 
+	
+	public static final String charset = "utf-8";
+	// 用户平台API账号(非登录账号,示例:N1234567)
+	public static String account = "N5676872";
+	// 用户平台API密码(非登录密码)
+	public static String pswd = "erduiCESHI17";
+	
 	// 2.发送短信
 
-	public static String sendMsg(String mobile) {
+	public static String aa(String mobile) {
 		
 		String url = "http://send.18sms.com/msg/HttpBatchSendSM";
 		Map<String,String> params = new HashMap<String,String> ();
@@ -53,6 +65,42 @@ public class SMSUtil {
 		return code;
 
 	}
+	
+	public static String sendMsg(String mobile) {
+
+		//请求地址请登录253云通讯自助通平台查看或者询问您的商务负责人获取
+		String smsSingleRequestServerUrl = " http://smssh1.253.com/msg/send/json";
+		// 短信内容
+		String msg = "您的验证码是：{code}。"+
+				"感谢您注册示远科技会员，为了您的安全请及时去更换您的初始密码，如有疑问请联系：4007761818。"+
+				"感谢您的注册示远科技会员，验证码{code}，请及时输入验证码进行登陆。"+
+				"您的帐号密码为：4007761818，请查收，详情客服电话4007761818。"+
+				"您的手机号：18858103840验证码888168，一天内提交有效，如不是本人操作请忽略此短信。"+
+				"您好，您的验证码888168，5分钟内有效。";
+		String code = autoRandomCode(6);
+		msg = msg.replace("{code}", code);
+		//手机号码
+		String phone = mobile;
+		//状态报告
+		String report= "true";
+		
+		SmsSendRequest smsSingleRequest = new SmsSendRequest(account, pswd, msg, phone,report);
+		
+		String requestJson = JSON.toJSONString(smsSingleRequest);
+		
+		
+		
+		String response = ChuangLanSmsUtil.sendSmsByPost(smsSingleRequestServerUrl, requestJson);
+		
+		System.out.println("response after request result is :" + response);
+		
+		SmsSendResponse smsSingleResponse = JSON.parseObject(response, SmsSendResponse.class);
+		
+		return code;
+
+	}
+	
+	
 	public static String autoRandomCode(int length){
 		Random  d = new Random();
 		String str = "";
